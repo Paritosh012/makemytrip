daba9ed251db28d69d7157712352f00b64340380
Paritosh Jaiswal <jaiswalparitosh8@gmail.com>
2026-05-17 23:42:12 +0530

    NEW

--- backend/utils/email.js
+++ backend/utils/email.js
@@ -1,16 +1,36 @@
 const nodemailer = require("nodemailer");
+const { google } = require("googleapis");
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com", // explicit host instead of service:"gmail"
-  port: 587, // ← 587 works on Render, 465 is blocked
-  secure: false, // false for 587 (STARTTLS)
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS,
-  },
-});
+const OAuth2 = google.auth.OAuth2;
+
+const createTransporter = async () => {
+  const oauth2Client = new OAuth2(
+    process.env.GMAIL_CLIENT_ID,
+    process.env.GMAIL_CLIENT_SECRET,
+    "https://developers.google.com/oauthplayground"
+  );
+
+  oauth2Client.setCredentials({
+    refresh_token: process.env.GMAIL_REFRESH_TOKEN,
+  });
+
+  const accessToken = await oauth2Client.getAccessToken();
+
+  return nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      type: "OAuth2",
+      user: process.env.EMAIL_USER,
+      clientId: process.env.GMAIL_CLIENT_ID,
+      clientSecret: process.env.GMAIL_CLIENT_SECRET,
+      refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+      accessToken: accessToken.token,
+    },
+  });
+};
 
 const sendEmail = async (to, subject, html) => {
+  const transporter = await createTransporter();
   await transporter.sendMail({
     from: `"TravelSaaS" <${process.env.EMAIL_USER}>`,
     to,
